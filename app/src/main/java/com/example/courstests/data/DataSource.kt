726f064e251d75dbfb2269bae1def15d4d6dd030
EdouardMc <104469772+RobertEdMc@users.noreply.github.com>
2025-05-27 15:00:48 -0400

    Creacion de los Objetos Cousers

---
+++ app/src/main/java/com/example/courstests/data/DataSource.kt
@@ -0,0 +1,22 @@
+package com.example.courstests.data
+import com.example.courstests.R
+import com.example.courstests.model.DataCourse
+
+object DataSource {
+    val courses = listOf(
+        DataCourse(R.string.architecture, 58, R.drawable.architecture),
+        DataCourse(R.string.crafts, 121, R.drawable.crafts),
+        DataCourse(R.string.business, 78, R.drawable.business),
+        DataCourse(R.string.culinary, 118, R.drawable.culinary),
+        DataCourse(R.string.design, 423, R.drawable.design),
+        DataCourse(R.string.fashion, 92, R.drawable.fashion),
+        DataCourse(R.string.film, 165, R.drawable.film),
+        DataCourse(R.string.gaming, 164, R.drawable.gaming),
+        DataCourse(R.string.drawing, 326, R.drawable.drawing),
+        DataCourse(R.string.lifestyle, 305, R.drawable.lifestyle),
+        DataCourse(R.string.music, 212, R.drawable.music),
+        DataCourse(R.string.painting, 172, R.drawable.painting),
+        DataCourse(R.string.photography, 321, R.drawable.photography),
+        DataCourse(R.string.tech, 118, R.drawable.tech)
+    )
+}
