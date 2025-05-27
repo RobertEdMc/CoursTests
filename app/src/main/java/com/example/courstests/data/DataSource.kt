51ea28653e593f597c3a0775ce7b2ef658142e90
EdouardMc <104469772+RobertEdMc@users.noreply.github.com>
2025-05-27 16:19:26 -0400

    Añadiendo mas texto y añadiendo imagen de grain

--- app/src/main/java/com/example/courstests/data/DataSource.kt
+++ app/src/main/java/com/example/courstests/data/DataSource.kt
@@ -5,18 +5,29 @@ import com.example.courstests.model.DataCourse
 object DataSource {
     val courses = listOf(
         DataCourse(R.string.architecture, 58, R.drawable.architecture),
+        DataCourse(R.string.architecture, 58, R.drawable.architecture),
+        DataCourse(R.string.automotive, 30, R.drawable.automotive),
+        DataCourse(R.string.biology, 90, R.drawable.biology),
         DataCourse(R.string.crafts, 121, R.drawable.crafts),
         DataCourse(R.string.business, 78, R.drawable.business),
         DataCourse(R.string.culinary, 118, R.drawable.culinary),
         DataCourse(R.string.design, 423, R.drawable.design),
+        DataCourse(R.string.ecology, 28, R.drawable.ecology),
+        DataCourse(R.string.engineering, 67, R.drawable.engineering),
         DataCourse(R.string.fashion, 92, R.drawable.fashion),
+        DataCourse(R.string.finance, 100, R.drawable.finance),
         DataCourse(R.string.film, 165, R.drawable.film),
-        DataCourse(R.string.gaming, 164, R.drawable.gaming),
+        DataCourse(R.string.gaming, 37, R.drawable.gaming),
+        DataCourse(R.string.geology, 290, R.drawable.geology),
         DataCourse(R.string.drawing, 326, R.drawable.drawing),
+        DataCourse(R.string.history, 189, R.drawable.history),
+        DataCourse(R.string.journalism, 96, R.drawable.journalism),
+        DataCourse(R.string.law, 58, R.drawable.law),
         DataCourse(R.string.lifestyle, 305, R.drawable.lifestyle),
         DataCourse(R.string.music, 212, R.drawable.music),
         DataCourse(R.string.painting, 172, R.drawable.painting),
         DataCourse(R.string.photography, 321, R.drawable.photography),
-        DataCourse(R.string.tech, 118, R.drawable.tech)
+        DataCourse(R.string.physics, 41, R.drawable.physics),
+        DataCourse(R.string.tech, 118, R.drawable.tech),
     )
 }
